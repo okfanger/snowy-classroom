72b32a5bc895f4c72e38a76208f6406bb53286b8
fang <lovefyj616@foxmail.com>
2022-08-26 10:22:09 +0800

    chore: 新增antd pro

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,26 +1,28 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
+import { constantRouterMap } from '@/config/router.config'
 
-Vue.use(VueRouter);
+// hack router push callback
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
 
-const routes = [
-  {
-    path: "",
-    name: "Layout",
-    component: () => import("@/components/WebLayout"),
-    redirect: "/home",
-    children: [
-      {
-        path: "home",
-        name: "Home",
-        component: () => import("@/views/HomeView"),
-      },
-    ],
-  },
-];
+Vue.use(Router)
 
-const router = new VueRouter({
-  routes,
-});
+const createRouter = () =>
+  new Router({
+    mode: 'history',
+    routes: constantRouterMap
+  })
 
-export default router;
+const router = createRouter()
+
+// 定义一个resetRouter 方法，在退出登录后或token过期后 需要重新登录时，调用即可
+export function resetRouter () {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher
+}
+
+export default router
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,12 +1,29 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+import app from './modules/app'
+import user from './modules/user'
+
+// default router permission control
+// 默认路由模式为静态路由 (router.config.js)
+import permission from './modules/permission'
+
+// dynamic router permission control (Experimental)
+// 动态路由模式（api请求后端生成）
+// import permission from './modules/async-router'
+
+import getters from './getters'
+
+Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    app,
+    user,
+    permission
+  },
   state: {},
-  getters: {},
   mutations: {},
   actions: {},
-  modules: {},
-});
+  getters
+})
