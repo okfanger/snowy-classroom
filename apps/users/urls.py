3462dfd3f09a6350369238c969f1d44e2bf2c966
fang <lovefyj616@foxmail.com>
2022-09-01 16:34:49 +0800

    chore: 后端大换血+1

---
+++ apps/users/urls.py
@@ -0,0 +1,34 @@
+"""back URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from apps.users.authentication import MyTokenObtainPairView
+
+urlpatterns = [
+    # 获取/刷新/验证 token
+    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # 获取Token的接口
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # 刷新Token有效期的接口
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # 验证Token的有效性
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
