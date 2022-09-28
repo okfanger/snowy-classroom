0c58cfe6742d762a0008eafb787762a7c61aaffb
fang <lovefyj616@foxmail.com>
2022-09-28 12:42:17 +0800

    chore: 配置更新

--- front/src/config/defaultSettings.js
+++ front/src/config/defaultSettings.js
@@ -13,11 +13,12 @@
  */
 
 export default {
-  navTheme: 'dark', // theme for nav menu
+  theme: 'light',
+  navTheme: 'light', // theme for nav menu
   primaryColor: '#1890ff', // '#F5222D', // primary color of ant design
-  layout: 'sidemenu', // nav menu position: `sidemenu` or `topmenu`
-  contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
-  fixedHeader: false, // sticky header
+  layout: 'topmenu', // nav menu position: `sidemenu` or `topmenu`
+  contentWidth: 'Fixed', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
+  fixedHeader: true, // sticky header
   fixSiderbar: false, // sticky siderbar
   colorWeak: false,
   menu: {
