f7ddab739d3e3cc9f962525fb8bf1ddc40722406
fang <lovefyj616@foxmail.com>
2022-10-10 16:42:14 +0800

    feat: 教师学生管理员 权限类

---
+++ apps/users/permissions.py
@@ -0,0 +1,44 @@
+from rest_framework.permissions import BasePermission
+
+
+# (1, '学生'), (2, '教师'), (3, '超级管理员')
+class IsStudent(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.type:
+            return False
+        if request.user.type != 1:
+            return False
+        else:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return True
+
+
+class IsTeacher(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.type:
+            return False
+        if request.user.type != 2:
+            return False
+        else:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return True
+
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.type:
+            return False
+        if request.user.type != 3:
+            return False
+        else:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return True
