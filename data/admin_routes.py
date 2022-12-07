189f7259dac98748b0c5272289f9881192752c74
xh <xuhuan1129@qq.com>
2022-12-07 23:57:17 +0800

    feat：管理员端完善

--- data/admin_routes.py
+++ data/admin_routes.py
@@ -26,5 +26,18 @@ admin_role_routes = [
     'component': 'admin/overall/index.vue',
     'redirect': ''
 },
+    {
+        'name': 'homework',  # 路由名
+        'parentId': 0,  # 父菜单ID，如果没有父菜单就写0
+        'id': 71003,  # 路由ID
+        "path": "homework",  # 路由地址（显示在浏览器URL上的）
+        'meta': {  # 元信息
+            'title': 'menu.homework.look',  # 国际化标识，可以在前端locales里增加
+            'icon': 'book',  # 图标，这个可以从Ant Design Vue 组件库中找
+            'show': True,  # 是否显示
+        },
+        'component': 'admin/homework/homework.vue',  # 前端.vue文件的相对地址（views为根目录，最前面不用写/）
+        'redirect': ''  # 重定向，如果这边填了其实component就没啥用，不需要重定向就留空字符串即可
+    },
 
 ]
--- front/src/locales/lang/zh-CN/xh.js
+++ front/src/locales/lang/zh-CN/xh.js
@@ -2,6 +2,7 @@ export default {
   'menu.overall': '整体',
   'menu.homework.push': '交作业',
   'menu.homework': '作业',
-  'menu.sendHomework': '发布作业'
+  'menu.sendHomework': '发布作业',
+  'menu.homework.look': '查看作业'
 
 }
