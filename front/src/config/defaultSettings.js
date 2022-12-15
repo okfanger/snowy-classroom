340409bb68c5efd5f23ca5e909128393c59d8028
fang <lovefyj616@foxmail.com>
2022-12-15 09:58:34 +0800

    chore: 更改前端配置

--- front/src/config/defaultSettings.js
+++ front/src/config/defaultSettings.js
@@ -13,11 +13,11 @@
  */
 
 export default {
-  theme: 'dark',
-  navTheme: 'dark', // theme for nav menu
+  theme: 'light',
+  navTheme: 'light', // theme for nav menu
   primaryColor: '#1890ff', // '#F5222D', // primary color of ant design
-  layout: 'sidemenu', // nav menu position: `sidemenu` or `topmenu`
-  contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
+  layout: 'topmenu', // nav menu position: `sidemenu` or `topmenu`
+  contentWidth: 'Fixed', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
   fixedHeader: true, // sticky header
   fixSiderbar: true, // sticky siderbar
   colorWeak: false,
