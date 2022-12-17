9ec7564ee33e1a59e96ecca4c4523bcf24b615ee
谷志妍 <2961929072@qq.com>
2022-12-17 12:14:11 +0800

    fix: 固定 allure演示地址

--- scripts/allure-test.py
+++ scripts/allure-test.py
@@ -4,4 +4,4 @@ from back.settings import BASE_DIR
 allure_tmp_dir = os.path.join(BASE_DIR, "tests", "allure-results")
 os.system("del /s /q %s/*.*" % allure_tmp_dir)
 os.system(f"pytest --alluredir={allure_tmp_dir}")
-os.system(f"allure serve {allure_tmp_dir}")
+os.system(f"allure serve {allure_tmp_dir}  -h 127.0.0.1 -p 8091")
